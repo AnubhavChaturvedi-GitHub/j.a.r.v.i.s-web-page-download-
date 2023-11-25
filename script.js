de7ad40c835030dc51471e8fdb187a271e5bc277
Anubhav Chaturvedi <129895858+anubhavcha@users.noreply.github.com>
2023-11-25 11:49:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    // You can add the logic to initiate the download here
+    alert("Downloading J.A.R.V.I.S AI...");
+});
